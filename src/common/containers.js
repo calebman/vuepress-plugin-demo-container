03eab0b2a6f75eee442d39aec1583ad63bf7769c
chenzhangxiang <chenzhangxiang@bwton.com>
2022-01-14 21:03:04 +0800

    fix(./src/common/containers.js): 解决引入代码段无法正确渲染的问题
    fix #14

--- src/common/containers.js
+++ src/common/containers.js
@@ -1,5 +1,5 @@
+const fs = require('fs');
 const mdContainer = require('markdown-it-container');
-
 module.exports = options => {
   const {
     component = 'demo-block'
@@ -16,7 +16,15 @@ module.exports = options => {
         const m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
         if (tokens[idx].nesting === 1) {
           const description = m && m.length > 1 ? m[1] : '';
-          const content = tokens[idx + 1].type === 'fence' ? tokens[idx + 1].content : '';
+          let content = ''
+          if (tokens[idx + 1].type === 'fence') {
+            if (tokens[idx + 1].content) {
+              content = tokens[idx + 1].content
+            } else if (tokens[idx + 1].src) {
+              // 根据路径加载文件
+              content = fs.readFileSync(tokens[idx + 1].src, 'utf-8')
+            }
+          }
           const encodeOptionsStr = encodeURI(JSON.stringify(options));
           return `<${componentName} :options="JSON.parse(decodeURI('${encodeOptionsStr}'))">
             <template slot="demo"><!--pre-render-demo:${content}:pre-render-demo--></template>
